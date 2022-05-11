cc359345ae7fd48a84671d38727edf461dae75f7
ThibaultDct <thibault.doucet44@gmail.com>
2022-05-11 15:12:26 +0200

    :tada: Initial commit

---
+++ components/Sidebar.vue
@@ -0,0 +1,24 @@
+<!-- Please remove this file from your project -->
+<template>
+    <div class="mt-8 bg-primary overflow-hidden shadow sm:rounded-lg p-2 float-left w-20">
+        
+      <div id="sidebar-top">
+
+      </div>
+
+      <div id="sidebar-content">
+
+      </div>
+
+      <div id="sidebar-bottom">
+
+      </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <span>
+    <Sidebar />
+    <Tutorial/>
+  </span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
